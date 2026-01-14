79d89f5c65e4a57971980586c4d73dce98469327
EthanZ4 <ezimmer3@nait.ca>
2026-01-14 11:28:23 -0700

    innerHTML review

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -2,20 +2,33 @@
 let topicList = document.querySelector(".topics-list")
 let newTopicForm = document.querySelector(".new-topic-form")
 
+// 5. create a function addTopicToPage that will take a name and list elements
+const addTopicToPage = (topicName, topicListElement) => {
+    // 6a. create a new inner list element and update the HTML
+    let newTopicElement = `<li class="list-group-item">
+        ${topicName}
+    </li>`
+    topicListElement.innerHTML += newTopicElement
+}
+
 // 2. add event listener, stop default formm submit event (would reload page)
-newTopicForm.addEventListener('submit', (event) => { 
+newTopicForm.addEventListener(
+    "submit", 
+    (event) => { 
     
-    event.preventDefault()
-    
-    // 3. grab input element & extract/store value
-    let topicInput = event.target.elements['new-topic']
-    let newTopic = topicInput.value
-    console.log(newTopic)
-    
-    // 4. validation - check for empty, use bootstrap classes for user feedback
-    if (newTopic === "") {
-        topicInput.classList.add('is-invalid')
-    } else {
-        topicInput.classList.remove('is-invalid')
-    }
-})
+        event.preventDefault()
+        
+        // 3. grab input element & extract/store value
+        let topicInput = event.target.elements["new-topic"]
+        let newTopic = topicInput.value
+        console.log(newTopic)
+        
+        // 4. validation - check for empty, use bootstrap classes for user feedback
+        if (newTopic === "") {
+            topicInput.classList.add("is-invalid")
+        } else {
+            topicInput.classList.remove("is-invalid")
+        }
+        // 6b. don't forget to call the function inside the event Listener
+        addTopicToPage(newTopic, topicList)
+    })
