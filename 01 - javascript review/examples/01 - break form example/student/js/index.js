3c0737771307d1af3d3743cab67c4d02781dc171
EthanZ4 <ezimmer3@nait.ca>
2026-01-14 10:57:23 -0700

    event review

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -1,8 +1,21 @@
-/*
-Enter JS here
+// 1. select topic & new topic form
+let topicList = document.querySelector(".topics-list")
+let newTopicForm = document.querySelector(".new-topic-form")
 
-HTML for list topic list item
-<li class="list-group-item">
-    NEW TOPIC HERE
-</li>
-*/
+// 2. add event listener, stop default formm submit event (would reload page)
+newTopicForm.addEventListener('submit', (event) => { 
+    
+    event.preventDefault()
+    
+    // 3. grab input element & extract/store value
+    let topicInput = event.target.elements['new-topic']
+    let newTopic = topicInput.value
+    console.log(newTopic)
+    
+    // 4. validation - check for empty, use bootstrap classes for user feedback
+    if (newTopic === "") {
+        topicInput.classList.add('is-invalid')
+    } else {
+        topicInput.classList.remove('is-invalid')
+    }
+})
